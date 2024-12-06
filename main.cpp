442ba57009a3b2e6ebc7a07e3795868f0d42c52d
GianmarcoDiRusso <g.dirusso@studenti.uniba.it>
2024-12-06 12:28:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,18 @@ int main() {
     dict.inserisci("apple", "fruit");
     dict.inserisci("car", "vehicle");
     dict.inserisci("book", "object");
+    
+   
 
+    
     std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
     std::cout << "Value for 'car': " << dict.recupera("car") << "\n";
     std::cout << "Value for 'pen': " << dict.recupera("pen") << "\n";
 
     dict.stampa();
+    dict.cancella("car");
+    dict.appartiene("apple");
 
+    
     return 0;
 }
